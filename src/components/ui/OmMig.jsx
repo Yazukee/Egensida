2c0374346faa7478ea39f83b3fd630dd9ec3db4f
Yazukee <yasser.almaliki@live.se>
2025-03-17 11:08:44 +0100

    justering

--- src/components/ui/OmMig.jsx
+++ src/components/ui/OmMig.jsx
@@ -7,18 +7,35 @@ function OmMig() {
       <h2 className="text-3xl font-bold mb-8 text-center">Om mig</h2>
       <div className="max-w-4xl mx-auto md:flex-row items-center gap-10">
         <p className="text-lg leading-relaxed">
-          Att <b>våga gå sin egen väg</b> är Yasers största fråga, varför så
-          många väljer att inte följa sina drömmar. I sin bok{" "}
-          <b>Kostnaden att inte välja sin väg </b>
-          delar han med <b>livsnycklar</b> och <b> insikter</b> för att
-          inspirera andra att <b>våga</b> ha modet trots rädslan att gå för det
-          man drömmer om. <br />
+          Att{" "}
+          <u>
+            <b>våga gå sin egen väg</b>
+          </u>{" "}
+          är Yasers största fråga, varför så många väljer att inte följa sina
+          drömmar. I sin bok{" "}
+          <u>
+            <b>Kostnaden att inte välja sin väg</b>
+          </u>{" "}
+          delar han med{" "}
+          <u>
+            <b>livsnycklar</b>
+          </u>{" "}
+          och{" "}
+          <u>
+            <b>insikter</b>
+          </u>{" "}
+          för att inspirera andra att{" "}
+          <u>
+            <b>våga</b>
+          </u>{" "}
+          ha modet trots rädslan att gå för det man drömmer om. <br />
           Det va aldrig menat att någon skulle vara hjälte eller komma och rädda
           dig i din historia, <br /> du är den som var menad att var hjälten i
           din historia!
           <br />
           <br />
         </p>
+
         <h2 className="font-bold text-2xl">En ledare från ung ålder</h2>
         <p className="text-lg leading-relaxed">
           Uppväxten präglades av basket, år och timmar investerade i sporten
@@ -26,38 +43,69 @@ function OmMig() {
           <br /> Redan i gymnasiet blev Yaser invald i advisory board för
           Stockholms Handelskammare och utmärkte sig tidigt som en av Fryshusets
           stjärnskott. <br />
-          <b>
-            Han har varit ambassadör för Fryshuset för Robert F. Kennedy Center
-            For Justice And Human Rights
-          </b>
+          <u>
+            <b>
+              Han har varit ambassadör för Fryshuset för Robert F. Kennedy
+              Center For Justice And Human Rights
+            </b>
+          </u>
           . Var en av de unga representanterna vid Prince Charles första
-          officiella besök i Sverige tillsammans med svenska kungafamiljen. Han
-          <b>
-            {" "}
-            representerade även Fryshuset vid Chiles första statsbesök i Sverige
-            med president Michelle Bachelet.
-          </b>
+          officiella besök i Sverige tillsammans med svenska kungafamiljen. Han{" "}
+          <u>
+            <b>
+              representerade även Fryshuset vid Chiles första statsbesök i
+              Sverige med president Michelle Bachelet.
+            </b>
+          </u>
         </p>
         <br />
+
         <h2 className="font-bold text-2xl">
           Engagemang för samhället och näringslivet
         </h2>
         <p className="text-lg leading-relaxed">
           Yaser var en drivande kraft bakom Fryshusets initiativ att få unga i
           arbete. Han har också fått en utmärkelse från{" "}
-          <b> Karl XVI Gustaf för sitt unga ledarskap</b>. <br /> Han har varit
+          <u>
+            <b>Karl XVI Gustaf för sitt unga ledarskap</b>
+          </u>
+          . <br /> Han har varit{" "}
+          <u>
+            <b>
+              styrelseledamot i Raoul Wallenberg Academy och var med och
+              utvecklade Kubprojektet
+            </b>
+          </u>
+          , en utbildningsinsats för att sprida kunskap om medmänsklighet. Vid{" "}
+          <u>
+            <b>
+              20 års ålder blev Yaser en av de yngsta medlemmarna i Rotary i
+              Sverige och har sedan dess haft flera styrelseuppdrag, inklusive
+              som president i Stockholm Strand Rotaryklubb
+            </b>
+          </u>
+        </p>
+        <br />
+
+        <h2 className="font-bold text-3xl">
+          Från försäljning till fintech och entreprenörskap
+        </h2>
+        <p className="text-lg leading-relaxed">
+          Med en{" "}
           <b>
-            {" "}
-            styrelseledamot i Raoul Wallenberg Academy och var med och
-            utvecklade Kubprojektet
-          </b>
-          , en utbildningsinsats för att sprida kunskap om medmänsklighet. Vid
-          <b>
-            {" "}
-            20 års ålder blev Yaser en av de yngsta medlemmarna i Rotary i
-            Sverige och har sedan dess haft flera styrelseuppdrag, inklusive som
-            president i Stockholm Strand Rotaryklubb
-          </b>
+            <u>examen iföretagsekonomi från Stockholms universitet</u>
+          </b>{" "}
+          och en examen som{" "}
+          <u>
+            <b>fullstack-utvecklare</b>
+          </u>{" "}
+          har Yaser kombinerat affärsförståelse med teknisk kompetens. Han har
+          arbetat inom försäljning av privatlån, försäkringar, företagslån och
+          var på fintech-uppstickaren Froda, ett av Sveriges ledande
+          fintech-bolag.{" "}
+          <u>
+            <b>Dessutom har han drivit två bolag</b>
+          </u>
         </p>
       </div>
     </section>
