d0c4e99fb9014ff162e056aaf42b795ff2c46dd1
Yazukee <yasser.almaliki@live.se>
2025-03-17 10:26:19 +0100

    justeringar

--- src/components/ui/OmMig.jsx
+++ src/components/ui/OmMig.jsx
@@ -1,13 +1,11 @@
 function OmMig() {
   return (
-    <section id="about" className="py-20 px-6 md:px-20 bg-black text-white">
+    <section
+      id="about"
+      className="py-20 px-6 md:px-20 bg-black text-white flex flex-col"
+    >
       <h2 className="text-3xl font-bold mb-8 text-center">Om mig</h2>
-      <div className="max-w-4xl mx-auto flex flex-col md:flex-row items-center gap-10">
-        {/* <img
-          src="./assets/portratt.jpg"
-          alt="Föreläsare"
-          className="w-60 h-60 object-cover rounded-full shadow-lg"
-        /> */}
+      <div className="max-w-4xl mx-auto md:flex-row items-center gap-10">
         <p className="text-lg leading-relaxed">
           Att <b>våga gå sin egen väg</b> är Yasers största fråga, varför så
           många väljer att inte följa sina drömmar. I sin bok{" "}
@@ -18,6 +16,48 @@ function OmMig() {
           Det va aldrig menat att någon skulle vara hjälte eller komma och rädda
           dig i din historia, <br /> du är den som var menad att var hjälten i
           din historia!
+          <br />
+          <br />
+        </p>
+        <h2 className="font-bold text-2xl">En ledare från ung ålder</h2>
+        <p className="text-lg leading-relaxed">
+          Uppväxten präglades av basket, år och timmar investerade i sporten
+          trots att han spelade på elitnivå började han även coacha.
+          <br /> Redan i gymnasiet blev Yaser invald i advisory board för
+          Stockholms Handelskammare och utmärkte sig tidigt som en av Fryshusets
+          stjärnskott. <br />
+          <b>
+            Han har varit ambassadör för Fryshuset för Robert F. Kennedy Center
+            For Justice And Human Rights
+          </b>
+          . Var en av de unga representanterna vid Prince Charles första
+          officiella besök i Sverige tillsammans med svenska kungafamiljen. Han
+          <b>
+            {" "}
+            representerade även Fryshuset vid Chiles första statsbesök i Sverige
+            med president Michelle Bachelet.
+          </b>
+        </p>
+        <br />
+        <h2 className="font-bold text-2xl">
+          Engagemang för samhället och näringslivet
+        </h2>
+        <p className="text-lg leading-relaxed">
+          Yaser var en drivande kraft bakom Fryshusets initiativ att få unga i
+          arbete. Han har också fått en utmärkelse från{" "}
+          <b> Karl XVI Gustaf för sitt unga ledarskap</b>. <br /> Han har varit
+          <b>
+            {" "}
+            styrelseledamot i Raoul Wallenberg Academy och var med och
+            utvecklade Kubprojektet
+          </b>
+          , en utbildningsinsats för att sprida kunskap om medmänsklighet. Vid
+          <b>
+            {" "}
+            20 års ålder blev Yaser en av de yngsta medlemmarna i Rotary i
+            Sverige och har sedan dess haft flera styrelseuppdrag, inklusive som
+            president i Stockholm Strand Rotaryklubb
+          </b>
         </p>
       </div>
     </section>
